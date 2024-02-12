c3513d81953c14a95e286dd436ea20bf2f4c226a
dilaraguven <87075779+dilaraguven@users.noreply.github.com>
2024-02-12 16:50:32 +0300

    2.commit

---
+++ src/HipotenusBulma.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HipotenusBulma {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Hipotenusunun hesaplanmasını istediginiz ucgenin kenarlarını giriniz");
+        System.out.println("1.kenar:");
+        int kenar1= sc.nextInt();
+        System.out.println("2.kenar");
+        int kenar2=sc.nextInt();
+
+        double hipotenus=Math.sqrt((kenar1*kenar1)+(kenar2*kenar2));
+
+        System.out.println("Kenarları girilen ucgenin hipotenusu: "+hipotenus);
+    }
+}
